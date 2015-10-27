64d01cf7e5dc09c61d46f8857e7250d6b870b3e9
joconnell7511 <jennifer.anne7511@gmail.com>
2015-10-26 18:48:02 -0700

    gallery/list functions
    messing around with team-controller.js and team.js

--- js/controllers/team-view-controller.js
+++ js/controllers/team-view-controller.js
@@ -16,4 +16,27 @@ var addForm = function(title){
 
 
     //set the content of each element
+    this.header.innerHTML = <h3>'+title+'</h3><div class ="button add"></div>;
     this.addForm.innerHTML = '<input type="text">';
+    this.addForm.classList.add('close');
+
+    // append the elements to the container
+    this.elem.appendChild(this.header);
+    this.elem.appendChild(this.addForm);
+    this.elem.appendChild(this.ul);
+
+    // select the element with a class 'add' and make it open the form and focus the input field
+       this.header.querySelectorAll('.add')[0].addEventListener('click',function(ev){
+            this.addForm.classList.remove('close');
+            this.addForm.classList.add('open');
+            this.addForm.querySelectorAll('input')[0].focus();
+       }.bind(this));
+
+       // function that adds an item
+    Accordian.prototype.addItem = function(item) {
+      // push the item to the list
+      this.list.push(item);
+      // create a li
+      var listItem = document.createElement('li');
+      // set the content of the li
+      listItem.innerHTML = '<span>'+item.content+'</span>';
